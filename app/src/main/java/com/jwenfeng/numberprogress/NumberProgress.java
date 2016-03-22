cd0191f562231bfc704b8dd5479369a84cf5b77d
jinwenfeng <823546371@qq.com>
2016-03-22 22:21:15 +0800

    first commit

---
+++ app/src/main/java/com/jwenfeng/numberprogress/NumberProgress.java
@@ -0,0 +1,34 @@
+package com.jwenfeng.numberprogress;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 当前类注释:
+ * 项目名：NumberProgress
+ * 包名：com.jwenfeng.numberprogress
+ * 作者：jinwenfeng on 16/3/22 22:20
+ * 邮箱：823546371@qq.com
+ * QQ： 823546371
+ * 公司：南京穆尊信息科技有限公司
+ * © 2016 jinwenfeng
+ * ©版权所有，未经允许不得传播
+ */
+public class NumberProgress extends View {
+
+    public NumberProgress(Context context) {
+        this(context,null);
+    }
+
+    public NumberProgress(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public NumberProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+
+
+    }
+}
